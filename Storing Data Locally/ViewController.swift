7131866dc547959c9eb9a69ceec39e9d721a22a3
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-01-15 15:22:57 +0300

    final commit

--- Storing Data Locally/ViewController.swift
+++ Storing Data Locally/ViewController.swift
@@ -10,16 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var birthDayText: UITextField!
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+     let storedName = UserDefaults.standard.object(forKey: "namesurname")
+     let storedDate = UserDefaults.standard.object(forKey: "birthday")
+        
+    
+        if let newName = storedName as? String {
+            nameLabel.text = "Name: \(newName)"
+        }
+        
+        if let newDate = storedDate as? String {
+            birthdayLabel.text = "Birthday: \(newDate)"
+        }
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func deleteButtonClicked(_ sender: Any) {
+        
+        let storedName = UserDefaults.standard.object(forKey: "namesurname")
+        let storedDate = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let _ = storedName as? String {
+            UserDefaults.standard.removeObject(forKey: "namesurname")
+            UserDefaults.standard.synchronize()
+            
+            nameLabel.text = "Name: "
+            nameText.text = ""
+            
+        }
+        
+        if let _ = storedDate as? String {
+            
+            UserDefaults.standard.removeObject(forKey: "birthday")
+            UserDefaults.standard.synchronize()
+            
+            birthDayText.text = ""
+            birthdayLabel.text = "Birthday: "
+            
+        }
+
+        
     }
 
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        UserDefaults.standard.set(nameText.text, forKey: "namesurname")
+        UserDefaults.standard.set(birthDayText.text, forKey: "birthday")
+        UserDefaults.standard.synchronize()
+        
+        nameLabel.text = "Name: \(nameText.text!)"
+        birthdayLabel.text = "Birthday: \(birthDayText.text!)"
+    }
+ 
 
 }
 
